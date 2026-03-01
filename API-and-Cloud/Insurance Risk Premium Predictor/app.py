831276de5561efe1fa43927cd07e8741845037c0
Vikash Kumar <signifiedvikash@gmail.com>
2026-03-01 18:46:18 +0530

    refining the structure

--- API-and-Cloud/Insurance Risk Premium Predictor/app.py
+++ API-and-Cloud/Insurance Risk Premium Predictor/app.py
@@ -1,73 +1,40 @@
 import numpy as np
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field, computed_field, ValidationError
+from pydantic import BaseModel, Field, computed_field, ValidationError, field_validator
 import json
 import pandas as pd
 import pickle
 from typing import List, Optional, Literal, Annotated
 from fastapi.responses import JSONResponse
 
+
+
+#importing the user_input module from the schema package to use the UserInput class for data validation and processing
+import schema.user_input 
+
+
 #impoting the model
-with open(r"F:\Python\FastAPI01\model.pkl", "rb") as file:
+with open(r"F:\Python\FastAPI01\API-and-Cloud\Insurance Risk Premium Predictor\model.pkl", "rb") as file:
     model = pickle.load(file)
+
+model_version = "1.0.0"
 
 app = FastAPI()
 
-tier1 = ['Delhi','Mumbai','Bangalore','Hyderabad','Kolkata',]
-tier2 = ['Jaipur', 'Chennai', 'Indore', 'Kota', 
-'Chandigarh', 'Pune',  'Lucknow', 'Gaya',
-'Jalandhar', 'Mysore']
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Health Premier Risk Prediction API. Use the /predict endpoint to get your health risk category based on your details."}
 
-class UserInput(BaseModel):
-    age: Annotated[int, Field(..., gt=0, description="The age of the user", examples=[30, 40])]
-    weight:Annotated[float, Field(..., gt = 0, description="The weight of the user in kg", examples=[70.5, 60.0])]
-    height: Annotated[float, Field(..., gt = 0,lt=2.5, description="The height of the user in cm", examples=[170.0, 165.5])]
-    income_lpa: Annotated[float, Field(..., ge=0, description="The income level of the user in LPA", examples=[10000, 25000])]
-    smoker: Annotated[bool, Field(..., description="Whether the user is a smoker", examples=[True, False])]
-    city: Annotated[str, Field(..., description="The city of the user", examples=["New York", "Los Angeles"])]
-    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-       'business_owner', 'unemployed', 'private_job'], Field(..., description="The occupation of the user", examples=["Engineer", "Teacher"])]
-    
-    @computed_field
-    @property
-    def bmi(self) -> float:
-        bmi = round(self.weight / ((self.height)**2),2)
-        return bmi
-    
-    @computed_field
-    @property
-    def age_group(self)-> str:
-         if self.age < 25:
-            return "young"
-         elif self.age < 45:
-            return "adult"
-         elif self.age<60:
-            return "middle_age"
-         else:
-            return "senior"
-         
+@app.get("/health")
+def health_check():
+    return {
+        "status": "API is healthy and running.",
+        "version": model_version,
+        "model loaded": model is not None
+        }
 
-    @computed_field
-    @property
-    def lifestyle_risk(self) -> str:
-        if self.bmi > 30 and self.smoker:
-            return "high"
-        elif self.bmi > 27 and self.smoker:
-            return "medium"
-        else:
-            return "low"
-        
-    @computed_field
-    @property
-    def tier(self) -> int:
-        if self.city in tier1:
-            return 1
-        elif self.city in tier2:
-            return 2
-        return 3
-    
 @app.post("/predict")
-def predict_risk(data: UserInput):
+def predict_risk(data: schema.user_input.UserInput):
 
     input_df = pd.DataFrame ([{
         "bmi": data.bmi,
---
+++ API-and-Cloud/Insurance Risk Premium Predictor/schema/user_input.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, Field, computed_field, field_validator
+import typing
+
+
+tier1 = ['Delhi','Mumbai','Bangalore','Hyderabad','Kolkata',]
+tier2 = ['Jaipur', 'Chennai', 'Indore', 'Kota', 
+'Chandigarh', 'Pune',  'Lucknow', 'Gaya',
+'Jalandhar', 'Mysore']
+
+class UserInput(BaseModel):
+    age: typing.Annotated[int, Field(..., gt=0, description="The age of the user", examples=[30, 40])]
+    weight:typing.Annotated[float, Field(..., gt = 0, description="The weight of the user in kg", examples=[70.5, 60.0])]
+    height: typing.Annotated[float, Field(..., gt = 0,lt=2.5, description="The height of the user in cm", examples=[170.0, 165.5])]
+    income_lpa: typing.Annotated[float, Field(..., ge=0, description="The income level of the user in LPA", examples=[10000, 25000])]
+    smoker: typing.Annotated[bool, Field(..., description="Whether the user is a smoker", examples=[True, False])]
+    city: typing.Annotated[str, Field(..., description="The city of the user", examples=["New York", "Los Angeles"])]
+    occupation: typing.Annotated[typing.Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'], Field(..., description="The occupation of the user", examples=["Engineer", "Teacher"])]
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight / ((self.height)**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def age_group(self)-> str:
+         if self.age < 25:
+            return "young"
+         elif self.age < 45:
+            return "adult"
+         elif self.age<60:
+            return "middle_age"
+         else:
+            return "senior"
+         
+
+    @computed_field
+    @property
+    def lifestyle_risk(self) -> str:
+        if self.bmi > 30 and self.smoker:
+            return "high"
+        elif self.bmi > 27 and self.smoker:
+            return "medium"
+        else:
+            return "low"
+        
+    @computed_field
+    @property
+    def tier(self) -> int:
+        if self.city in tier1:
+            return 1
+        elif self.city in tier2:
+            return 2
+        return 3
+    
+    @field_validator('city')
+    @classmethod
+    def normalize_city(cls, value):
+        return value.strip().title()
+    
